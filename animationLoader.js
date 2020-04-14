23680abb57ff289eec98dea76a6ecccf843a82c3
conwayhub <ofdermott@gmail.com>
2020-04-14 09:11:08 +0100

    more files

--- animationLoader.js
+++ animationLoader.js
@@ -1,14 +1,54 @@
 const butterfly = document.querySelector("#butterfly");
+const butterfly2 = document.querySelector("#butterfly2");
+const butterfly3 = document.querySelector("#butterfly3");
+const butterfly4 = document.querySelector("#butterfly4");
+
+const butterflyArray1 = [
+  "Mexican_Bluewing",
+  "Mother_Of_Pearl",
+  "Autumn_Leaf",
+  "Punchinello",
+];
+const butterflyArray2 = [
+  "butterfly4",
+  "butterfly5",
+  "butterfly6",
+  "butterfly14",
+];
+const butterflyArray3 = [
+  "butterfly7",
+  "butterfly8",
+  "butterfly9",
+  "butterfly15",
+];
+const butterflyArray4 = [
+  "butterfly10",
+  "butterfly11",
+  "butterfly11",
+  "butterfly12",
+];
 
 const randomiser = () => {
-  return Math.floor(Math.random() * 2) + 1;
+  return Math.floor(Math.random() * 4);
 };
 
 const thisValue = "butterfly.components.gltf-model.attrValue";
 setTimeout(() => {
   butterfly.setAttribute(
     "gltf-model",
-    `./butterflies/butterfly${randomiser()}.gltf`
+    `./butterflies/${butterflyArray1[randomiser()]}.gltf`
+  );
+  butterfly2.setAttribute(
+    "gltf-model",
+    `./butterflies/${butterflyArray2[randomiser()]}.gltf`
+  );
+  butterfly3.setAttribute(
+    "gltf-model",
+    `./butterflies/${butterflyArray3[randomiser()]}.gltf`
+  );
+  butterfly4.setAttribute(
+    "gltf-model",
+    `./butterflies/${butterflyArray4[randomiser()]}.gltf`
   );
 }, 500);
 
--- ar.js
+++ ar.js
@@ -1,20 +1,20 @@
 const targetEl = document.querySelector("#butterfly");
+const secondTargetEl = document.querySelector("#butterfly2");
+const thirdTargetEl = document.querySelector("#butterfly3");
+const fourthTargetEl = document.querySelector("#butterfly4");
 const textEl = document.querySelector("#text");
 const scanTextEl = document.querySelector("#promptToScan");
 const catchTextEl = document.querySelector("#promptToCatch");
 const gotItTextEl = document.querySelector("#catchedIt");
 const refObj = {};
-let catched = false;
+let catched1 = false;
+let catched2 = false;
+let catched3 = false;
+let catched4 = false;
 
-targetEl.addEventListener("click", function () {
-  console.log(refObj);
-  if (catched === false) {
-    addToRefObj(targetEl);
-    textEl.setAttribute("text", "value", `Caught: ${refObj[targetEl.id]}`);
-    targetEl.components.animation.attrValue.enabled = true;
-    catchTextEl.object3D.visible = false;
-    catched = true;
-  }
+targetEl.addEventListener("click", (event) => {
+  handleClick(targetEl, catched1);
+  catched1 = true;
 });
 
 targetEl.addEventListener("mouseenter", () => {
@@ -27,10 +27,86 @@ targetEl.addEventListener("mouseleave", () => {
   catchTextEl.object3D.visible = false;
 });
 
+secondTargetEl.addEventListener("click", (event) => {
+  handleClick(secondTargetEl, catched2);
+  catched2 = true;
+});
+secondTargetEl.addEventListener("mouseenter", () => {
+  scanTextEl.object3D.visible = false;
+  catchTextEl.object3D.visible = true;
+});
+
+secondTargetEl.addEventListener("mouseleave", () => {
+  setTimeout(setVisibility, 2000);
+  catchTextEl.object3D.visible = false;
+});
+
+thirdTargetEl.addEventListener("click", (event) => {
+  handleClick(thirdTargetEl, catched3);
+  catched3 = true;
+});
+thirdTargetEl.addEventListener("mouseenter", () => {
+  scanTextEl.object3D.visible = false;
+  catchTextEl.object3D.visible = true;
+});
+thirdTargetEl.addEventListener("mouseleave", () => {
+  setTimeout(setVisibility, 2000);
+  catchTextEl.object3D.visible = false;
+});
+fourthTargetEl.addEventListener("click", (event) => {
+  handleClick(fourthTargetEl, catched4);
+  catched4 = true;
+});
+
+fourthTargetEl.addEventListener("mouseenter", () => {
+  scanTextEl.object3D.visible = false;
+  catchTextEl.object3D.visible = true;
+});
+
+fourthTargetEl.addEventListener("mouseleave", () => {
+  setTimeout(setVisibility, 2000);
+  catchTextEl.object3D.visible = false;
+});
+
+function handleClick(element, catchValue) {
+  scanTextEl.object3D.visible = false;
+  catchTextEl.object3D.visible = false;
+  if (catchValue === false) {
+    addToRefObj(element);
+    textEl.setAttribute(
+      "value",
+      `You caught a ${element.classList}!
+    You can now view it in your collection!`
+    );
+    element.components.animation.attrValue.enabled = true;
+    setAsVisible(textEl);
+    setTimeout((event) => {
+      textEl.object3D.visible = false;
+    }, 2000);
+  } else {
+    textEl.setAttribute(
+      "value",
+      `You've already caught this ${element.classList}!`
+    );
+    element.components.animation.attrValue.enabled = true;
+    setAsVisible(textEl);
+    setTimeout((event) => {
+      textEl.object3D.visible = false;
+    }, 2000);
+  }
+}
+
 const setVisibility = () => {
   if (!catchTextEl.object3D.visible) {
     scanTextEl.object3D.visible = true;
   }
+};
+
+const setAsInisible = (element) => {
+  element.object3D.visible = false;
+};
+const setAsVisible = (element) => {
+  element.object3D.visible = true;
 };
 
 const addToRefObj = (target) => {
@@ -50,4 +126,4 @@ const addToRefObj = (target) => {
 //   height: 3,
 //   width: 1
 // });
-// targetEl.appendChild(newEntity);
+// targetEl.appendChild(newEntity)
